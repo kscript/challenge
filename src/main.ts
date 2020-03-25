89e796dde6db71297e94368c54b8136cfe8b9dc0
kscript <1258@live.cn>
2020-03-26 00:47:21 +0800

    添加eventBus

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import elementUI from 'element-ui'
+import eventBus from '@/eventbus'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+Vue.use(eventBus)
 Vue.use(elementUI)
 new Vue({
   router,
