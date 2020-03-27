b5c322a83cf5368f4564efd57c54273aab772b76
kscript <1258@live.cn>
2020-03-28 03:33:31 +0800

    修改输出目录

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'development' ? '/challenge/' : './',
-  outputDir: '/docs/',
+  outputDir: 'docs',
   devServer: {
     proxy: {
       '/api': {
